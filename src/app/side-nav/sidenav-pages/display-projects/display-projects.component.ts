f3395dd18473bf8bb3094d5eee8ab5c213aada68
Asadullah <casadullah49@gmail.com>
2023-07-20 20:51:11 +0500

    update 2.2

--- src/app/side-nav/sidenav-pages/display-projects/display-projects.component.ts
+++ src/app/side-nav/sidenav-pages/display-projects/display-projects.component.ts
@@ -7,90 +7,97 @@ import { ProjectService } from 'src/app/services/project.service';
   styleUrls: ['./display-projects.component.scss']
 })
 export class DisplayProjectsComponent {
-  projects: IProject[] = [];
-  searchedProjects: IProject[] = [];
-  selectedProjects: IProject[] = [];
+  projects: IProject[] = []; //to store all projects of user...
+  searchedProjects: IProject[] = []; //to store searched projects array....
+  selectedProjects: IProject[] = []; // to store projects based on selected tabs....
 
-  searched = '';
-  plength = 0;
-  completed = false;
+  searched = ''; //searched input value....
+  plength = 0; // length of searched projects array....
+  completed = false; 
   viewAll = false;
   inProgress = false;
 
-  constructor(
-    public projectService: ProjectService
-    ){
+  //custroctor.....
+  constructor(public projectService: ProjectService){}
+
+  //OnInIt....
+  ngOnInit(){
+    //storing projects in this.projects[]......
+    this.viewAll = true;
+    this.getProjects();
+    this.selectedProjects = this.projects;
+  }
+
+  //do check .......
+  ngDoCheck(){
+    
+    if(this.searched!==''){
+      this.searchedProjects = [];
+      this.plength = 0;
+      this.projects.forEach(project=>{
+        if(project.title === this.searched){
+          this.searchedProjects.push(
+            project
+          )
+        }
+      })
+      this.plength = this.searchedProjects.length;
     }
-    ngOnInit(){
-      //storing projects in this.projects[]......
-      this.viewAll = true;
-      this.getProjects();
-      this.selectedProjects = this.projects;
-    }
-    ngDoCheck(){
-      
-      if(this.searched!==''){
-        this.searchedProjects = [];
-        this.plength = 0;
-        this.projects.forEach(project=>{
-          if(project.title === this.searched){
-            this.searchedProjects.push(
-              project
+    
+  }
+
+  //get project to get all projects of user....
+  async getProjects(){
+    this.projects = [];
+    await this.projectService.getProjects()
+    .then(qDocs => qDocs.forEach(
+            qDoc =>  this.projects.push(
+              qDoc.data() as IProject
             )
-          }
-        })
-        this.plength = this.searchedProjects.length;
+          )
+        )
+  }
+
+  //to show all available project.....
+  showAll(){
+    this.viewAll = true;
+    this.completed = false;
+    this.inProgress = false;
+    this.selectedProjects = this.projects;
+  }
+
+  //to show completed projects....
+  showCompleted(){
+    this.completed = true;
+    this.inProgress = false;
+    this.viewAll = false;
+    
+    this.selectedProjects = [];
+    
+    this.projects.forEach(project=>{
+      if(project.status){
+        this.selectedProjects.push(
+          project
+        )
       }
-      
-    }
+    })
+  }
 
-    async getProjects(){
-      this.projects = [];
-     await this.projectService.getProjects()
-      .then(qDocs => qDocs.forEach(
-              qDoc =>  this.projects.push(
-                qDoc.data() as IProject
-              )
-            )
-          )
-    }
+  //to show inProgress projects...
+  showInProgress(){
+    this.inProgress = true;
+    this.viewAll = false;
+    this.completed = false;
+    
+    this.selectedProjects = [];
 
-    showAll(){
-      this.viewAll = true;
-      this.completed = false;
-      this.inProgress = false;
-      this.selectedProjects = this.projects;
-    }
-    showCompleted(){
-      this.completed = true;
-      this.inProgress = false;
-      this.viewAll = false;
-      
-      this.selectedProjects = [];
-      
-      this.projects.forEach(project=>{
-        if(project.status){
-          this.selectedProjects.push(
-            project
-          )
-        }
-      })
-    }
-
-    showInProgress(){
-      this.inProgress = true;
-      this.viewAll = false;
-      this.completed = false;
-      
-      this.selectedProjects = [];
-
-      this.projects.forEach(project=>{
-        if(!project.status){
-          this.selectedProjects.push(
-            project
-            
-          )
-        }
-      })
-    }
+    this.projects.forEach(project=>{
+      if(!project.status){
+        this.selectedProjects.push(
+          project
+          
+        )
+      }
+    })
+  }
 }
